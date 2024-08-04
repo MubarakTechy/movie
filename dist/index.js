65720d1e4df4608a210b436d8dbfdc0d88dcb0f5
mubaraktechy <punkmk6@gmail.com>
2024-08-03 18:49:18 -0700

    done with movies wesite

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+document.querySelector(".toggle").addEventListener("click", () => {
+  document.querySelector(".toggle").classList.toggle("open");
+  document.querySelector(".hide-mobile-nav").classList.toggle("mobile-nav");
+//   document.querySelector(".EVP").style.opacity = "50%";
+});
